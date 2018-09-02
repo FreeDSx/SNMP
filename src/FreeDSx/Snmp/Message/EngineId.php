b23835d3aecaf4ef647995e6a9927b5cde68122c
Chad Sikorra <Chad.Sikorra@gmail.com>
2018-09-02 11:13:52 -0500

    Add an object to represent various EngineId formats.

---
+++ src/FreeDSx/Snmp/Message/EngineId.php
@@ -0,0 +1,430 @@
+<?php
+/**
+ * This file is part of the FreeDSx SNMP package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FreeDSx\Snmp\Message;
+
+use FreeDSx\Snmp\Exception\UnexpectedValueException;
+
+/**
+ * Represents the SnmpEngineId and associated format.
+ *
+ * @see https://tools.ietf.org/html/rfc3411#section-5
+ * @author Chad Sikorra <Chad.Sikorra@gmail.com>
+ */
+class EngineId
+{
+    /**
+     * The Private Enterprise Number assigned to FreeDSx SNMP.
+     *
+     * @see https://www.iana.org/assignments/enterprise-numbers/enterprise-numbers
+     */
+    public const ENTERPRISE_NUMBER = 52564;
+
+    public const FORMAT_IPV4 = 1;
+
+    public const FORMAT_IPV6 = 2;
+
+    public const FORMAT_MAC = 3;
+
+    public const FORMAT_TEXT = 4;
+
+    public const FORMAT_OCTET = 5;
+
+    protected static $octetDelim = ' ';
+
+    /**
+     * @var null|int
+     */
+    protected $format;
+
+    /**
+     * @var int
+     */
+    protected $enterpriseId;
+
+    /**
+     * @var string
+     */
+    protected $data;
+
+    /**
+     * @var null|string
+     */
+    protected $binary;
+
+    /**
+     * @param int $enterpriseId
+     * @param $data
+     * @param null|int $format
+     */
+    public function __construct($data, ?int $format, int $enterpriseId = self::ENTERPRISE_NUMBER)
+    {
+        $this->data = $data;
+        $this->format = $format;
+        $this->enterpriseId = $enterpriseId;
+    }
+
+    /**
+     * Get the decoded representation of the data based on the format.
+     *
+     * @return string
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEnterpriseId() : int
+    {
+        return $this->enterpriseId;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getFormat() : ?int
+    {
+        return $this->format;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVariableLengthFormat() : bool
+    {
+        return ($this->format !== null);
+    }
+
+    /**
+     * @return string
+     */
+    public function toBinary()
+    {
+        if ($this->binary === null) {
+            $enterpriseId = ($this->format !== null) ? ($this->enterpriseId | 0x80000000) : $this->enterpriseId;
+            $enterpriseId = hex2bin(str_pad(dechex($enterpriseId), 8, "0", STR_PAD_LEFT));
+
+            if ($this->format !== null) {
+                $this->binary = $enterpriseId.chr($this->format).self::encodeDataFormat($this->format, $this->data);
+            } else {
+                $this->binary = $enterpriseId.str_pad($this->data, 8, "\x00", STR_PAD_LEFT);
+            }
+        }
+
+        return $this->binary;
+    }
+
+    /**
+     * @return string
+     */
+    public function toHex() : string
+    {
+        return bin2hex($this->toBinary());
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toHex();
+    }
+
+    /**
+     * Construct the EngineID object based on a binary representation.
+     *
+     * @param $engineId
+     * @return EngineId
+     */
+    public static function fromBinary($engineId) : EngineId
+    {
+        return self::parse($engineId);
+    }
+
+    /**
+     * Construct the EngineID object based on an IPv4 format.
+     *
+     * @param string $ipAddress
+     * @param int $enterpriseNumber
+     * @return EngineId
+     */
+    public static function fromIPv4(string $ipAddress, int $enterpriseNumber = self::ENTERPRISE_NUMBER) : EngineId
+    {
+        return new self($ipAddress, self::FORMAT_IPV4, $enterpriseNumber);
+    }
+
+    /**
+     * Construct the EngineID object based on an IPv6 format.
+     *
+     * @param string $ipAddress
+     * @param int $enterpriseNumber
+     * @return EngineId
+     */
+    public static function fromIPv6(string $ipAddress, int $enterpriseNumber = self::ENTERPRISE_NUMBER) : EngineId
+    {
+        return new self($ipAddress, self::FORMAT_IPV6, $enterpriseNumber);
+    }
+
+    /**
+     * Construct the EngineID object based on a MAC format.
+     *
+     * @param string $mac
+     * @param int $enterpriseNumber
+     * @return EngineId
+     */
+    public static function fromMAC(string $mac, int $enterpriseNumber = self::ENTERPRISE_NUMBER) : EngineId
+    {
+        return new self($mac, self::FORMAT_MAC, $enterpriseNumber);
+    }
+
+    /**
+     * Construct the EngineID object based on a text format.
+     *
+     * @param string $text
+     * @param int $enterpriseNumber
+     * @return EngineId
+     */
+    public static function fromText(string $text, int $enterpriseNumber = self::ENTERPRISE_NUMBER) : EngineId
+    {
+        return new self($text, self::FORMAT_TEXT, $enterpriseNumber);
+    }
+
+    /**
+     * Construct the EngineID object based on an octet format.
+     *
+     * @param string $octets
+     * @param int $enterpriseNumber
+     * @return EngineId
+     */
+    public static function fromOctet(string $octets, int $enterpriseNumber = self::ENTERPRISE_NUMBER) : EngineId
+    {
+        return new self($octets, self::FORMAT_OCTET, $enterpriseNumber);
+    }
+
+    /**
+     * Construct an engineId using the RFC1910 method, which has no standardized format (just 8 bytes of data)
+     *
+     * @param string $data
+     * @param int $enterpriseNumber
+     * @return EngineId
+     */
+    public static function fromRFC1910($data, int $enterpriseNumber = self::ENTERPRISE_NUMBER) : EngineId
+    {
+        return new self($data, null, $enterpriseNumber);
+    }
+
+    /**
+     * @param $engineId
+     * @return EngineId
+     */
+    protected static function parse($engineId) : EngineId
+    {
+        $length = strlen($engineId);
+        # The engine ID must be between 5 and 32 bytes long
+        if ($length < 5 || $length > 32) {
+            throw new UnexpectedValueException('The engine ID length is invalid.');
+        }
+        # It cannot be composed of all H'00' or H'FF'
+        if (str_repeat("\x00", $length) === $engineId || str_repeat("\xff", $length) === $engineId) {
+            throw new UnexpectedValueException('The engine ID is malformed.');
+        }
+        $format = null;
+        $enterpriseId = hexdec(bin2hex(substr($engineId, 0, 4)));
+
+        # The first bit specifies whether it is variable length format from RFC3411...
+        if (ord($engineId[0]) >= 128) {
+            $format = ord($engineId[4]);
+            # The first bit of the ID is an identifier for variable length format, so flip it.
+            $enterpriseId ^= 0x80000000;
+            $data = self::parseDataFormat($format, substr($engineId, 5));
+        } else {
+            $data = substr($engineId, 4);
+        }
+
+        return new self($data, $format, $enterpriseId);
+    }
+
+    /**
+     * @param int $format
+     * @param $data
+     * @return mixed
+     */
+    protected static function parseDataFormat(int $format, $data)
+    {
+        if ($format === self::FORMAT_IPV4) {
+            $data = self::parseIPv4($data);
+        } elseif ($format === self::FORMAT_IPV6) {
+            $data = self::parseIPv6($data);
+        } elseif ($format === self::FORMAT_MAC) {
+            $data = self::parseMAC($data);
+        } elseif ($format === self::FORMAT_TEXT) {
+            $data = (string) $data;
+        } elseif ($format === self::FORMAT_OCTET) {
+            $data = bin2hex($data);
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param int $format
+     * @param $data
+     * @return bool|string
+     */
+    protected static function encodeDataFormat(int $format, $data)
+    {
+        if ($format === self::FORMAT_IPV4) {
+            $data = self::encodeIPv4($data);
+        } elseif ($format === self::FORMAT_IPV6) {
+            $data = self::encodeIPv6($data);
+        } elseif ($format === self::FORMAT_MAC) {
+            $data = self::encodeMAC($data);
+        } elseif ($format === self::FORMAT_OCTET) {
+            $data = self::encodeOctets($data);
+        }
+
+        return $data;
+    }
+
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected static function encodeOctets($data)
+    {
+        if (strpos($data, self::$octetDelim) !== false) {
+            $data = explode(self::$octetDelim, $data);
+            foreach ($data as $i => $piece) {
+                $data[$i] = str_pad($piece, 2, '0', STR_PAD_LEFT);
+            }
+            $data = implode('', $data);
+        }
+        if (!ctype_xdigit($data)) {
+            throw new UnexpectedValueException('The octets contains invalid values.');
+        }
+        if (strlen($data) % 2) {
+            throw new UnexpectedValueException('The octets must be an even length.');
+        }
+
+        return hex2bin($data);
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    protected static function parseIPv4($data) : string
+    {
+        $length = strlen($data);
+        if ($length !== 4) {
+            throw new UnexpectedValueException(sprintf('Expected 4 bytes for IPv4, got %s.', $length));
+        }
+
+        $ipAddress = [];
+        for ($i = 0; $i < 4; $i++) {
+            $ipAddress[] = ord($data[$i]);
+        }
+
+        return implode('.', $ipAddress);
+    }
+
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected static function encodeIPv4(string $data)
+    {
+        if (!filter_var($data, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
+            throw new UnexpectedValueException(sprintf('The IPv4 address is invalid: %s', $data));
+        }
+        $pieces = explode('.', $data);
+
+        $encoded = '';
+        foreach ($pieces as $piece) {
+            $encoded .= chr((int) $piece);
+        }
+
+        return $encoded;
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    protected static function parseIPv6($data) : string
+    {
+        $length = strlen($data);
+        if ($length !== 12) {
+            throw new UnexpectedValueException(sprintf('Expected 12 bytes for IPv6, got %s.', $length));
+        }
+
+        $pieces = str_split($data, 2);
+        foreach ($pieces as $i => $piece) {
+            $pieces[$i] = ltrim(bin2hex($piece), '0');
+        }
+
+        return implode(':', $pieces);
+    }
+
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected static function encodeIPv6(string $data)
+    {
+        if (!filter_var($data, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            throw new UnexpectedValueException(sprintf('The IPv6 address is invalid: %s', $data));
+        }
+        $pieces = explode(':', $data);
+
+        $encoded = '';
+        foreach ($pieces as $piece) {
+            $piece = str_pad($piece, 4, '0', STR_PAD_LEFT);
+            $encoded .= hex2bin($piece);
+        }
+
+        return $encoded;
+    }
+
+    /**
+     * @param $data
+     * @return string
+     */
+    protected static function parseMAC($data) : string
+    {
+        $length = strlen($data);
+        if ($length !== 6) {
+            throw new UnexpectedValueException(sprintf('Expected 6 bytes for a MAC, got %s.', $length));
+        }
+
+        return implode(':', str_split(bin2hex($data), 2));
+    }
+
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected static function encodeMAC(string $data)
+    {
+        if (!filter_var($data, FILTER_VALIDATE_MAC)) {
+            throw new UnexpectedValueException(sprintf('The MAC is invalid: %s', $data));
+        }
+        $pieces = explode(':', $data);
+
+        foreach ($pieces as $i => $piece) {
+            $pieces[$i] = str_pad($piece, '2', '0', STR_PAD_LEFT);
+        }
+
+        return hex2bin(implode('', $pieces));
+    }
+}
---
+++ tests/spec/FreeDSx/Snmp/Message/EngineIdSpec.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * This file is part of the FreeDSx SNMP package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\FreeDSx\Snmp\Message;
+
+use FreeDSx\Snmp\Exception\UnexpectedValueException;
+use FreeDSx\Snmp\Message\EngineId;
+use PhpSpec\ObjectBehavior;
+
+class EngineIdSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith('10.43.1.235', EngineId::FORMAT_IPV4);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(EngineId::class);
+    }
+
+    function it_should_get_the_format()
+    {
+        $this->getFormat()->shouldBeEqualTo(EngineId::FORMAT_IPV4);
+    }
+
+    function it_should_get_the_enterprise_number()
+    {
+        $this->getEnterpriseId()->shouldBeEqualTo(EngineId::ENTERPRISE_NUMBER);
+    }
+
+    function it_should_get_whether_it_is_rfc3411_formatted_or_not()
+    {
+        $this->isVariableLengthFormat()->shouldBeEqualTo(true);
+    }
+
+    function it_should_get_the_hexadecimal_representation()
+    {
+        $this->toHex()->shouldBeEqualTo('8000cd54010a2b01eb');
+    }
+
+    function it_should_get_the_binary_representation_for_ipv4()
+    {
+        $this->toBinary()->shouldBeEqualTo(hex2bin('8000cd54010a2b01eb'));
+    }
+
+    function it_should_get_the_binary_representation_for_ipv6()
+    {
+        $this->beConstructedWith('fe80::214:4fff:fe80:e187', EngineId::FORMAT_IPV6);
+        $this->toBinary()->shouldBeEqualTo(hex2bin('8000cd5402fe80000002144ffffe80e187'));
+    }
+
+    function it_should_get_the_binary_representation_for_mac()
+    {
+        $this->beConstructedWith('08:00:27:18:f1:0e', EngineId::FORMAT_MAC);
+        $this->toBinary()->shouldBeEqualTo(hex2bin('8000cd540308002718f10e'));
+    }
+
+    function it_should_get_the_binary_representation_for_text()
+    {
+        $this->beConstructedWith('Fred', EngineId::FORMAT_TEXT);
+        $this->toBinary()->shouldBeEqualTo(hex2bin('8000cd540446726564'));
+    }
+
+    function it_should_get_the_binary_representation_for_octet()
+    {
+        $this->beConstructedWith('01 04 03 33', EngineId::FORMAT_OCTET);
+        $this->toBinary()->shouldBeEqualTo(hex2bin('8000cd540501040333'));
+    }
+
+    function it_should_pad_delimited_octets_as_needed_for_binary()
+    {
+        $this->beConstructedWith('0 1 02 03', EngineId::FORMAT_OCTET);
+        $this->toBinary()->shouldBeEqualTo(hex2bin('8000cd540500010203'));
+
+    }
+
+    function it_should_parse_binary_ipv4()
+    {
+        $this::fromBinary(hex2bin('8000cd54010a2b01eb'))->shouldBeLike(new EngineId('10.43.1.235', EngineId::FORMAT_IPV4));
+    }
+
+    function it_should_parse_binary_ipv6()
+    {
+        $this::fromBinary(hex2bin('8000cd5402fe80000002144ffffe80e187'))->shouldBeLike(new EngineId('fe80::214:4fff:fe80:e187', EngineId::FORMAT_IPV6));
+    }
+
+    function it_should_parse_binary_mac()
+    {
+        $this::fromBinary(hex2bin('8000cd540308002718f10e'))->shouldBeLike(new EngineId('08:00:27:18:f1:0e', EngineId::FORMAT_MAC));
+    }
+
+    function it_should_parse_binary_text()
+    {
+        $this::fromBinary(hex2bin('8000cd540446726564'))->shouldBeLike(new EngineId('Fred', EngineId::FORMAT_TEXT));
+    }
+
+    function it_should_parse_binary_octets()
+    {
+        $this::fromBinary(hex2bin('8000cd540501040333'))->shouldBeLike(new EngineId('01040333', EngineId::FORMAT_OCTET));
+    }
+
+    function it_should_parse_an_enterprise_specific_binary_format()
+    {
+        $this::fromBinary(hex2bin('80001f8880e9f66b557e52285b00000000'))->shouldBeLike(new EngineId(hex2bin('e9f66b557e52285b00000000'), 128, 8072));
+    }
+
+    function it_should_parse_binary_rfc1910()
+    {
+        $this::fromBinary(hex2bin('00001f88e9f66b557e52285b'))->shouldBeLike(new EngineId(hex2bin('e9f66b557e52285b'), null, 8072));
+    }
+
+    function it_should_be_constructed_using_ipv4()
+    {
+        $this::fromIPv4('192.168.1.1') -> shouldBeLike(new EngineId('192.168.1.1', EngineId::FORMAT_IPV4));
+    }
+
+    function it_should_be_constructed_using_ipv6()
+    {
+        $this::fromIPv6('fe80::214:4fff:fe80:e187') -> shouldBeLike(new EngineId('fe80::214:4fff:fe80:e187', EngineId::FORMAT_IPV6));
+    }
+
+    function it_should_be_constructed_using_mac()
+    {
+        $this::fromMAC('08:00:27:18:f1:0e') -> shouldBeLike(new EngineId('08:00:27:18:f1:0e', EngineId::FORMAT_MAC));
+    }
+
+    function it_should_be_constructed_using_text()
+    {
+        $this::fromText('Fred') -> shouldBeLike(new EngineId('Fred', EngineId::FORMAT_TEXT));
+    }
+
+    function it_should_be_constructed_using_octets()
+    {
+        $this::fromOctet('00 01 02 03') -> shouldBeLike(new EngineId('00 01 02 03', EngineId::FORMAT_OCTET));
+    }
+
+    function it_should_be_constructed_using_rfc1910()
+    {
+        $this::fromRFC1910('Fred')->shouldBeLike(new EngineId('Fred', null));
+    }
+
+    function it_should_throw_an_exception_if_it_is_all_00()
+    {
+        $this->shouldThrow(new UnexpectedValueException('The engine ID is malformed.'))->during('fromBinary', [hex2bin('000000000000')]);
+    }
+
+    function it_should_throw_an_excecption_if_it_is_all_FF()
+    {
+        $this->shouldThrow(new UnexpectedValueException('The engine ID is malformed.'))->during('fromBinary', [hex2bin('ffffffffffff')]);
+    }
+
+    function it_should_throw_an_exception_on_invalid_ipv4_length_in_binary()
+    {
+        $this->shouldThrow(new UnexpectedValueException('Expected 4 bytes for IPv4, got 5.'))->during('fromBinary', [hex2bin('8000cd54010a2b01ebff')]);
+        $this->shouldThrow(new UnexpectedValueException('Expected 4 bytes for IPv4, got 3.'))->during('fromBinary', [hex2bin('8000cd54010a2b01')]);
+    }
+
+    function it_should_throw_an_exception_on_invalid_ipv4()
+    {
+        $this->beConstructedWith('0.0.0', EngineId::FORMAT_IPV4);
+        $this->shouldThrow(new UnexpectedValueException('The IPv4 address is invalid: 0.0.0'))->during('toBinary');
+    }
+
+    function it_should_throw_an_exception_on_invalid_ipv6_length_in_binary()
+    {
+        $this->shouldThrow(new UnexpectedValueException('Expected 12 bytes for IPv6, got 13.'))->during('fromBinary', [hex2bin('8000cd5402fe80000002144ffffe80e187ff')]);
+        $this->shouldThrow(new UnexpectedValueException('Expected 12 bytes for IPv6, got 11.'))->during('fromBinary', [hex2bin('8000cd5402fe80000002144ffffe80e1')]);
+    }
+
+    function it_should_throw_an_exception_on_invalid_ipv6()
+    {
+        $this->beConstructedWith('12:33a2:::', EngineId::FORMAT_IPV6);
+        $this->shouldThrow(new UnexpectedValueException('The IPv6 address is invalid: 12:33a2:::'))->during('toBinary');
+    }
+
+    function it_should_throw_an_exception_on_invalid_mac_length_in_binary()
+    {
+        $this->shouldThrow(new UnexpectedValueException('Expected 6 bytes for a MAC, got 7.'))->during('fromBinary', [hex2bin('8000cd540308002718f10eaa')]);
+        $this->shouldThrow(new UnexpectedValueException('Expected 6 bytes for a MAC, got 5.'))->during('fromBinary', [hex2bin('8000cd540308002718f1')]);
+    }
+
+    function it_should_throw_an_exception_on_invalid_mac()
+    {
+        $this->beConstructedWith('1:2:3:4:5:6', EngineId::FORMAT_MAC);
+        $this->shouldThrow(new UnexpectedValueException('The MAC is invalid: 1:2:3:4:5:6'))->during('toBinary');
+    }
+
+    function it_should_throw_an_exception_on_invalid_octets()
+    {
+        $this->beConstructedWith('01za', EngineId::FORMAT_OCTET);
+        $this->shouldThrow(new UnexpectedValueException('The octets contains invalid values.'))->during('toBinary');
+    }
+
+    function it_should_throw_an_exception_on_uneven_length_octets()
+    {
+        $this->beConstructedWith('a1a', EngineId::FORMAT_OCTET);
+        $this->shouldThrow(new UnexpectedValueException('The octets must be an even length.'))->during('toBinary');
+    }
+}
