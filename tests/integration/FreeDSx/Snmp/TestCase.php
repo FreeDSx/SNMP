c8657e5f1950ace32c49f21230e5799f0bb69386
Chad Sikorra <Chad.Sikorra@gmail.com>
2021-12-26 09:50:17 -0600

    Start to add basic integration tests.

---
+++ tests/integration/FreeDSx/Snmp/TestCase.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace integration\FreeDSx\Snmp;
+
+use FreeDSx\Snmp\SnmpClient;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    public function makeReadOnlyClient(array $options = []): SnmpClient
+    {
+        return new SnmpClient(array_merge(
+            $this->defaultOptions(),
+            $options
+        ));
+    }
+
+    private function defaultOptions(): array
+    {
+        return [
+            'version' => 2,
+            'community' => getenv('SNMP_COMMUNITY_RO'),
+            'user' => getenv('SNMP_USER'),
+            'use_auth' => true,
+            'auth_pwd' => getenv('SNMP_PASS_USER'),
+            'auth_mech' => 'md5',
+            'use_priv' => false,
+            'priv_mech' => 'des',
+            'priv_pwd' => getenv('SNMP_PASS_PRIV'),
+            'host' => getenv('SNMP_SERVER'),
+            'port' => (int) getenv('SNMP_PORT'),
+        ];
+    }
+}
